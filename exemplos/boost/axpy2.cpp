2f5c2b3517249c2c3e17c013376f40fe6c28aa10
shifu117 <47926669+shifu117@users.noreply.github.com>
2021-06-22 22:41:39 +0100

    Update axpy2.cpp

--- exemplos/boost/axpy2.cpp
+++ exemplos/boost/axpy2.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include "../../lei/boost/boost_vector.cpp"
 
+namespace lei {
+
 struct Pair {
     double x;
     double y;
@@ -64,4 +66,5 @@ int main(int argc, char** argv){
          << duration.count() << " microseconds" << std::endl;
 
 	return 0;
+}
 }
